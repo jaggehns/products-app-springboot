f67543ab703e287b9922c006e6720f21a22ca7de
Jaggehn <jaggehns@gmail.com>
2024-09-24 14:27:50 +0800

    fix: indents

--- src/main/java/com/jaggehn/entity/Product.java
+++ src/main/java/com/jaggehn/entity/Product.java
@@ -14,54 +14,54 @@ import org.hibernate.annotations.UpdateTimestamp;
 @Entity
 @Table(name="PRODUCT_TBL")
 public class Product {
-	
-	@Id
-	@GeneratedValue
-	private int id;
-	private String name;
-	private int quantity;
-	private double price;
-	private String description;
-	
-	@CreationTimestamp
+    
+    @Id
+    @GeneratedValue
+    private int id;
+    private String name;
+    private int quantity;
+    private double price;
+    private String description;
+    
+    @CreationTimestamp
     @Column(updatable = false)
     private LocalDateTime createdAt;
 
     @UpdateTimestamp
     private LocalDateTime updatedAt;
-	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public int getQuantity() {
-		return quantity;
-	}
-	public void setQuantity(int quantity) {
-		this.quantity = quantity;
-	}
-	public double getPrice() {
-		return price;
-	}
-	public void setPrice(double price) {
-		this.price = price;
-	}
-	public String getDescription() {
-		return description;
-	}
-	public void setDescription(String description) {
-		this.description = description;
-	}
-	@Override
-	public String toString() {
-		return "Product [id=" + id + ", name=" + name + ", quantity=" + quantity + ", price=" + price + ", description=" + description + "]";
-	}
+    
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getQuantity() {
+        return quantity;
+    }
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    @Override
+    public String toString() {
+        return "Product [id=" + id + ", name=" + name + ", quantity=" + quantity + ", price=" + price + ", description=" + description + "]";
+    }
 }
